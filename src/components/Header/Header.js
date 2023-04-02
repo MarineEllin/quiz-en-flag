214a9973c25400f8c1569e05c7f3391ea5c8a1e9
Marine Ellin <marine.ellin@gmail.com>
2023-04-02 14:49:14 +0200

    adapt header size and replace text with power button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,18 @@
 import styles from "./Header.module.scss";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { languageState, loadingGameState } from "../../recoilState/atom";
+import { useSetRecoilState } from "recoil";
+import { loadingGameState } from "../../recoilState/atom";
 
 function Header() {
   const setLoadingGame = useSetRecoilState(loadingGameState);
-  const language = useRecoilValue(languageState);
   return (
     <div
       className={`${styles.headerContainer} d-flex flex-column align-items-center py-20`}
     >
-      <img className={`${styles.logo} my-20`} src="/logo.jpeg" alt="logo" />
-      <button onClick={() => setLoadingGame(true)} className="btn-header">
-        {language === "fr" ? "Recommencer" : "Restart"}
-      </button>
+      <img className={`${styles.logo} mb-10`} src="/logo.jpeg" alt="logo" />
+      <i
+        className={`${styles.power} fa-solid fa-power-off`}
+        onClick={() => setLoadingGame(true)}
+      ></i>
     </div>
   );
 }
